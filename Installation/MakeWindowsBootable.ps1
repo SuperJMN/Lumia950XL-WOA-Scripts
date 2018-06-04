dc04e0c0e4200a78b3569395a21eb31fbd879689
José Manuel Nieto <superjmn@outlook.com>
2018-06-04 10:39:41 +0200

    Abort if System drive letter cannot be obtained

--- Installation/MakeWindowsBootable.ps1
+++ Installation/MakeWindowsBootable.ps1
@@ -1,8 +1,17 @@
 $volume = Get-Volume | Where { $_.FileSystemType -eq 'NTFS' -and $_.FileSystemLabel -eq 'WindowsARM' } | Select -index 0
 $windowsDriveLetter = $volume.DriveLetter
+if ($windowsDriveLetter -eq $null)
+{
+	throw "Cannot get Windows drive letter"
+}
 
 $volume = Get-Volume | Where { $_.FileSystemType -eq 'FAT32' -and $_.FileSystemLabel -eq 'SYSTEM' } | Select -index 0
 $systemDriveLetter = $volume.DriveLetter
+
+if ($systemDriveLetter -eq $null)
+{
+	throw "Cannot get System partition drive Letter"
+}
 
 bcdboot "$($windowsDriveLetter):\windows" /l en-US /f UEFI /s "$($systemDriveLetter):"
 bcdedit /store "$($systemDriveLetter):\EFI\Microsoft\Boot\BCD" /set `{default`} testsigning on

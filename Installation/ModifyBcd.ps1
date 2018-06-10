f2db7be44205a8d3602b1454b93c3995dc38cd41
José Manuel Nieto <superjmn@outlook.com>
2018-06-10 18:30:12 +0200

    Fixed problem setting the partition to the BootShim entry

--- Installation/ModifyBcd.ps1
+++ Installation/ModifyBcd.ps1
@@ -8,6 +8,8 @@ function SetupBootShimEntry()
 	$guid = $output|%{$_.split(' ')[2]}
 
 	$tmp = & bcdedit /store $bcdFileName /set $guid path \EFI\boot\BootShim.efi
+
+    $bootMgrPartitionPath = GetBootMgrPartitionPath $bcdFileName
 	$tmp = & bcdedit /store $bcdFileName /set $guid device partition=$bootMgrPartitionPath
 	$tmp = & bcdedit /store $bcdFileName /set $guid testsigning on 
 	$tmp = & bcdedit /store $bcdFileName /set $guid nointegritychecks on
@@ -17,8 +19,6 @@ function SetupBootShimEntry()
 
 function SetupBootMgrEntry() 
 {
-	$bootMgrPartitionPath = GetBootMgrPartitionPath $bcdFileName
-
 	$tmp = & bcdedit /store $bcdFileName /set `{bootmgr`} displaybootmenu on
 	$tmp = & bcdedit /store $bcdFileName /deletevalue `{bootmgr`} customactions 
 	$tmp = & bcdedit /store $bcdFileName /deletevalue `{bootmgr`} custom:54000001 

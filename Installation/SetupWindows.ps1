91cf94105ce63d3198328b66feae7cac8a71778e
José Manuel Nieto <superjmn@outlook.com>
2018-06-10 20:50:06 +0200

    Fixed typo

--- Installation/SetupWindows.ps1
+++ Installation/SetupWindows.ps1
@@ -6,4 +6,4 @@ Scripted-Step "Windows 10 ARM image deployment" "$($PSScriptRoot)\DeployWindows.
 Scripted-Step "Drivers deployment" "$($PSScriptRoot)\InstallDrivers.ps1"
 Scripted-Step "We'll make the Windows partition bootable" "$($PSScriptRoot)\MakeWindowsBootable.ps1"
 Step "Reboot your phone. Select BootShim. Windows 10 ARM Setup should begin!`nPlease, COMPLETE THE WINDOWS SETUP until it boots to the Desktop screen.`nAfter it boots to the Desktop, please REBOOT (from Windows) and go to Mass Storage Mode again."
-Scripted-Step "We  going to enable Dual Boot.`nPlease, switch to Mass Storage Mode before proceeding." "$($PSScriptRoot)\EnableDualBoot.ps1"
+Scripted-Step "We are going to enable Dual Boot.`nPlease, switch to Mass Storage Mode before proceeding." "$($PSScriptRoot)\EnableDualBoot.ps1"

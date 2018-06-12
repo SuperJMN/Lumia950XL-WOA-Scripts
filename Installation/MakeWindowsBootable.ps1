c73e304059fa82e65e88aeeaac106cef8b38462a
José Manuel Nieto <superjmn@outlook.com>
2018-06-12 15:21:41 +0200

    Removed unncessary change of partition type

--- Installation/MakeWindowsBootable.ps1
+++ Installation/MakeWindowsBootable.ps1
@@ -12,7 +12,4 @@ bcdboot "$($windowsDriveLetter):\windows" /l en-US /f UEFI /s "$($systemDriveLet
 bcdedit /store "$($systemDriveLetter):\EFI\Microsoft\Boot\BCD" /set `{default`} testsigning on
 bcdedit /store "$($systemDriveLetter):\EFI\Microsoft\Boot\BCD" /set `{default`} nointegritychecks on
 
-Write-Host "Setting partition type to ESP..."
-$sysVolume | Get-Partition | Set-Partition -GptType "{C12A7328-F81F-11D2-BA4B-00A0C93EC93B}"
-
 "Done!"
